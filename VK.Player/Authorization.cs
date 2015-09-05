23523812f7f9ce062e20267af9e058083f8c4f40
zadiran <konstantin.zadiran@gmail.com>
2015-09-05 23:09:54 +0300

    auth form

---
+++ VK.Player/Authorization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VK.Player
+{
+    public partial class Authorization : Form
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Authorization_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=000000000&display=popup&redirect_uri=https://oauth.vk.com/blank.html&scope=audio&response_type=token&v=5.37");
+        }
+    }
+}
---
+++ VK.Player/Music.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VK.Player
+{
+    public partial class Music : Form
+    {
+        public Music()
+        {
+            InitializeComponent();
+        }
+
+        private void Music_Load(object sender, EventArgs e)
+        {
+            new Authorization().Show();
+        }
+    }
+}
