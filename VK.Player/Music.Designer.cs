04b5858a04bba741e1642b34f175c6a00d7b66d6
zadiran <konstantin.zadiran@gmail.com>
2016-10-11 00:13:49 +0300

    VKP-7 Hide Download button

--- VK.Player/Music.Designer.cs
+++ VK.Player/Music.Designer.cs
@@ -77,6 +77,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Download";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Music
